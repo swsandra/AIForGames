99a105ada1bed311f2592742725174344ae8b924
Sandra <sandrav0609@gmail.com>
2018-10-21 18:26:46 -0400

    Origin and destination of ray (when drawed) in ObstacleAvoidance behavior seems ok, for some reason it still doesn't recognize obstacles.

--- Assets/ObstacleAvoidance.cs
+++ Assets/ObstacleAvoidance.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class ObstacleAvoidance : DSeek
 {
     //How far to avoid collision
-    float avoidDistance=5f;
+    float avoidDistance=4f;
     //Length of the collision ray
     float lookAhead=10f;
     //Collision ray vector
@@ -26,19 +26,27 @@ public class ObstacleAvoidance : DSeek
     {
         //Calculate target to delegate to seek
         //Calculate the collision ray vector
+        //Vector3 position = character.transform.position;
+        //Vector3 position = gameObject.transform.position;
         rayVector = character.velocity.normalized * lookAhead;
-
-        RaycastHit hit; //Acts as collision in book algorithm
-
+        //rayVector += position;
+        //Vector3 direction = rayVector - position;
+        //print(direction);
+        RaycastHit hit; //Acts as collision structure in book algorithm
+        Debug.DrawRay(character.transform.position,rayVector,Color.red,2,false);
         //Find collision
         if (Physics.Raycast(character.transform.position, rayVector, out hit, lookAhead))
         {
             targetPosition = hit.point + hit.normal * avoidDistance;
-            
+            print("target transform");
+            print(target.transform.position);
+            print("target calculated");
+            print(targetPosition);
             return base.GetSteering(targetPosition);
         }
 
-        return steering;
+        return base.GetSteering(target.transform.position);
+        //return steering;
     }
 
 }
